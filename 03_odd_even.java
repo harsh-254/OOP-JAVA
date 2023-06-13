d8afa24859954a8835bd412a2417e4e451ea74c6
harsh-254 <136074776+harsh-254@users.noreply.github.com>
2023-06-13 18:38:16 +0530

    Create 03_odd_even.java

---
+++ 03_odd_even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fibo_I_02{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            int x = scn.nextInt();
+            if(x%2 == 0){
+                System.out.println("Even");
+            }
+            else{
+                System.out.println("Odd");
+            }
+        }
+    }
+}
