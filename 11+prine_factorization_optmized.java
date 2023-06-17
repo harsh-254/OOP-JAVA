c9b444c024272e5821eb46ac50656285549ce355
harsh-254 <136074776+harsh-254@users.noreply.github.com>
2023-06-17 10:37:23 +0530

    Create 11+prine_factorization_optmized.java

---
+++ 11+prine_factorization_optmized.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class nn{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        for(int f=2;f*f<=n;f++){ // f stands for factor 
+            while(n%f == 0){
+                System.out.print(f + " ");
+                n = n/f;
+            }
+        }
+        if(n>1){
+            System.out.print(n + " ");
+        }
+    }
+}
