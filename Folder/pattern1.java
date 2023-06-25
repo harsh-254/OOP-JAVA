20e1840da8e2ce1dc7cc4c93d4b8aa6c423219f8
harsh-254 <136074776+harsh-254@users.noreply.github.com>
2023-06-25 10:07:59 +0530

    Update pattern1.java
    /* 
    
    *
    *       *
    *       *       *
    *       *       *       *
    *       *       *       *       *
    
    */

--- Folder/pattern1.java
+++ Folder/pattern1.java
@@ -5,7 +5,7 @@ public class Main{
         int n = scn.nextInt();
         
         int st = 1;
-        for(int i=1;i<=n;i++){
+        for(int i=1; i<=n;i++){
             for(int j=1;j<=st;j++){
                 System.out.print("*\t");
             }
