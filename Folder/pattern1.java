6fe6213ae6e94335a7949e626391eb4f642b31b4
harsh-254 <136074776+harsh-254@users.noreply.github.com>
2023-06-25 10:25:01 +0530

    Update pattern1.java
                    *
            *       *       *
    *       *       *       *       *
            *       *       *
                    *

--- Folder/pattern1.java
+++ Folder/pattern1.java
@@ -6,7 +6,7 @@ public class Main{
         
         int st = 1;
         for(int i=1; i<=n;i++){
-            for(int j=1;j<=st;j++){
+            for(int j=1;j<=st;j++) {
                 System.out.print("*\t");
             }
            
